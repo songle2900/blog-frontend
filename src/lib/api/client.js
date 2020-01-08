5541bd4b238ce7ad59fcd28071a324df0265531d
Young Min Song <ysong88@myseneca.ca>
2020-01-07 19:44:38 -0500

    Proxy setting

---
+++ src/lib/api/client.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const client = axios.create();
+
+/*
+    Global setting example:
+    
+    // Use API address elsewhere
+    client.defaults.baseURL = 'https://external-api-server.com/'
+
+    // Header setting
+    client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
+
+    // Interceptor Settings
+    axios.intercepter.response.use({
+        response => {
+            // Take specific action on successful request
+            return response;
+        },
+        error => {
+            // Take specific action on request failure
+            return Promise.reject(error);
+        }
+    })
+*/
