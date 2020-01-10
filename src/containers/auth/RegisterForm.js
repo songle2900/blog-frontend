6066b624cfcde6c844300712d8d0f08d4596a100
Young Min Song <ysong88@myseneca.ca>
2020-01-09 22:21:21 -0500

    Register form updated

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.register
+    const { form, auth, authError } = useSelector(({ auth }) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError
     }));
 
     // Input change event handler
@@ -23,13 +25,31 @@ const RegisterForm = () => {
 
     // Form registration event handler
     const onSubmit = e => {
-        e.preventDefault();
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            // TODO: error
+            return;
+        }
+        
     };
 
     // Initialize the form when the component is first rendered
     useEffect(() => {
         dispatch(initializeForm('register'));
     }, [dispatch]);
+
+    // register success / failure
+    useEffect(() => {
+        if (authError) {
+            console.log('Error');
+            console.log(authError);
+            return;
+        }
+        if (auth) {
+            console.log('Register Success');
+            console.log(auth);
+        }
+    }, [auth, authError]);
 
     return (
         <AuthForm type="register" form={form} onChange={onChange} onSubmit={onSubmit} />
