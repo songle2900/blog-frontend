69be5655aee8e7bf7159dddfd5871591aad7e9c3
Young Min Song <ysong88@myseneca.ca>
2020-01-11 21:37:18 -0500

    Stay logged in after refreshing

--- src/index.js
+++ src/index.js
@@ -9,16 +9,29 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
 
 // Create middleware for saga
 const sagaMiddleware = createSagaMiddleware();
-
 const store = createStore(
     rootReducer, 
     composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
+function loadUser() {
+    try {
+        const user = localStorage.getItem('user');
+        if (!user) return;  // Do nothing if not logged in
+
+        store.dispatch(tempSetUser(user));
+        store.dispatch(check());
+    } catch (e) {
+        console.log('localStorage is not working');
+    }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
     <Provider store={store}>
