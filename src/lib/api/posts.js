e0eece3dd86258d4069f078aa4bebfb560748ea5
Young Min Song <ysong88@myseneca.ca>
2020-01-16 15:45:26 -0500

    New API for update (use Update API instead of write API when editing)

--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -1,12 +1,14 @@
 import qs from 'qs';
 import client from './client';
 
+// Write API
 export const writePost = ({ title, body, tags }) =>
     client.post('/api/posts', { title, body, tags });
 
-
+// Read API
 export const readPost = id => client.get(`/api/posts/${id}`);
 
+// List API
 export const listPosts = ({ page, username, tag }) => {
     const queryString = qs.stringify({
         page,
@@ -14,4 +16,11 @@ export const listPosts = ({ page, username, tag }) => {
         tag
     });
     return client.get(`/api/posts?${queryString}`);
-}
+};
+
+// Update API
+export const updatePost = ({ id, title, body, tags }) => client.patch(`/api/posts/${id}`, {
+    title,
+    body,
+    tags
+});
